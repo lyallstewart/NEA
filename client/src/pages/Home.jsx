926f546b462f9fc36ac241d92c4e6f2bd694b7f1
Lyall Stewart <me@lyallstewart.com>
2024-12-29 23:48:14 +0000

    Minor Changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,14 +4,10 @@ const Home = () => {
   return (
     <>
       <Header title="Dashboard" />
-      <div className="content">
-        <div className="card">
+        <div className="content">
           <h2>Upcoming Events</h2>
-        </div>
-        <div className="card">
           <h2>Your Clubs</h2>
         </div>
-      </div>
     </>
   );
 };
--- client/src/pages/admin/UserManagement.jsx
+++ client/src/pages/admin/UserManagement.jsx
@@ -1,14 +1,19 @@
+import Header from "../../components/Header.jsx";
+
 const UserManagement = () => {
   return (
     <>
-      <div className="card">
-        <h2>Manage Allowlist</h2>
-        <h3>Allowlist Settings</h3>
-        <h3>Allowlist Import</h3>
-        <h3>Manual Allowlist</h3>
-      </div>
-      <div className="card">
-        <h2>Manage Existing Users</h2>
+      <Header title="User Management" />
+      <div className="content">
+        <div className="card">
+          <h2>Manage Allowlist</h2>
+          <h3>Allowlist Settings</h3>
+          <h3>Allowlist Import</h3>
+          <h3>Manual Allowlist</h3>
+        </div>
+        <div className="card">
+          <h2>Manage Existing Users</h2>
+        </div>
       </div>
     </>
   );
